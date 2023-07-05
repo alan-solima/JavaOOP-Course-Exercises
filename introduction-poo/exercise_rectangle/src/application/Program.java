95dda648aacfc1e1e13a0d395215993150b44991
Alan Solima <alann.soares@outlook.com>
2023-07-05 15:57:11 -0300

    Introduction to POO
    Introduction to POO

---
+++ introduction-poo/exercise_rectangle/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Rectangle;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Rectangle rectangle;
+		rectangle = new Rectangle();
+		
+		System.out.println("Enter rectangle width and height: ");
+		rectangle.width = sc.nextDouble();
+		rectangle.height = sc.nextDouble();		
+		
+		System.out.println(rectangle);
+		
+		sc.close();
+	}
+}
---
+++ introduction-poo/exercise_rectangle/src/module-info.java
@@ -0,0 +1,2 @@
+module exercises_POO {
+}
