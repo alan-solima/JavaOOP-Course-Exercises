d91b39e5428e3c5250b61c9ea9893370cc09136b
Alan Solima <alann.soares@outlook.com>
2023-07-12 16:04:59 -0300

    Date-time manipulation
    examples and exercises manipulating date-time

---
+++ specialTopics-date-time/example_dateTimeCalc/src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		LocalDate d04 = LocalDate.parse("2023-07-12");
+		LocalDateTime d05 = LocalDateTime.parse("2023-07-12T01:50:23");
+		Instant d06 = Instant.parse("2023-07-12T01:50:23Z");
+
+		LocalDate pastWeekLocalDate = d04.minusDays(7);
+		LocalDate nextWeekLocalDate = d04.plusDays(7);
+		
+		System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+		System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
+		
+		LocalDateTime pastWeekLocalDateTime = d05.minusDays(7);
+		LocalDateTime nextWeekLocalDateTime = d05.plusDays(7);
+		
+		System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+		System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+		
+		Instant pastWeekInstant = d06.minus(7, ChronoUnit.DAYS);
+		Instant nextWeekInstant = d06.plus(7, ChronoUnit.DAYS);
+		
+		System.out.println("pastWeekInstant = " + pastWeekInstant);
+		System.out.println("nextWeekInstant = " + nextWeekInstant);
+		
+		Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay());
+		Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+		Duration t3 = Duration.between(pastWeekInstant, d06);
+		Duration t4 = Duration.between(d06, pastWeekInstant);
+		
+		System.out.println("t1 days = " + t1.toDays());
+		System.out.println("t2 days = " + t2.toDays());
+		System.out.println("t3 days = " + t3.toDays());
+		System.out.println("t4 days = " + t4.toDays());
+	}
+}
