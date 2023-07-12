3973e047507a11acacfc5541e65f07c4ab9a3d4e
Alan Solima <alann.soares@outlook.com>
2023-07-12 14:40:52 -0300

    Date-time manipulation
    examples and exercises manipulating date-time

---
+++ specialTopics-date-time/example_dateTimeFormat/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		LocalDate d04 = LocalDate.parse("2023-07-12");
+		LocalDateTime d05 = LocalDateTime.parse("2023-07-12T11:50:23");
+		Instant d06 = Instant.parse("2023-07-12T11:50:23Z");
+
+		// https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
+		DateTimeFormatter fmt4 = DateTimeFormatter.ISO_DATE_TIME;
+		DateTimeFormatter fmt5 = DateTimeFormatter.ISO_INSTANT;
+		
+		System.out.println("d04 = " + d04.format(fmt1));
+		System.out.println("d04 = " + fmt1.format(d04));
+		System.out.println("d04 = " + d04.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		System.out.println("d05 = " + d05.format(fmt1));
+		System.out.println("d05 = " + d05.format(fmt2));
+		System.out.println("d05 = " + d05.format(fmt4));
+		
+		System.out.println("d06 = " + fmt3.format(d06));
+		System.out.println("d06 = " + fmt5.format(d06));
+	}
+
+}
